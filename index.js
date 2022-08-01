92b2bf6550003b2ad2e3bb2ba74fdf8f61d074c6
Jason Ballejo <ballejo19@gmail.com>
2022-07-31 23:10:17 -0400

    commit tree d7000c9ab9005b6c24c52db69bed8006f74db79f

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+function findMatching(takeArray, arg){
+    const theDrivers = takeArray.filter(function (word){
+        if (word.toLowerCase() === arg.toLowerCase()){
+            return true
+        }
+    })    
+        return theDrivers 
+} 
+
+function fuzzyMatch(array, argument){
+    const driverz = array.filter(function (value) {
+        if (value[0] === argument[0]) {
+            return true
+        }
+    })
+    return driverz
+}
+
+function matchName(arrays, args) {
+    const properties = arrays.filter(function (findMatch){
+        if (findMatch.name === args) {
+            return true
+        }
+    })
+    console.log(properties)
+    return properties
+}
